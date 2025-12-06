772bc5487ea751bef96ff1302fa1c69ab00d9125
avidersonline-ux <avidersonline@gmail.com>
2025-12-07 02:03:09 +0530

    Update save_product.js

--- src/save_product.js
+++ src/save_product.js
@@ -2,13 +2,20 @@ import { ProductIN } from "./models/ProductIN.js";
 import { ProductUS } from "./models/ProductUS.js";
 
 export async function saveProduct(p) {
-  const Model = p.currency === "INR" ? ProductIN : ProductUS;
+  try {
+    // choose collection by currency
+    const Model = p.currency === "INR" ? ProductIN : ProductUS;
 
-  await Model.updateOne(
-    { id: p.id },
-    { $set: p },
-    { upsert: true }
-  );
+    await Model.updateOne(
+      { id: p.id },
+      { $set: p },
+      { upsert: true }
+    );
 
-  return true;
+    return true;
+
+  } catch (err) {
+    console.error("Save error:", err.message);
+    return false;
+  }
 }
