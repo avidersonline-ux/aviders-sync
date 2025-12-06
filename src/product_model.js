ecd5a3c3f032b5f972ae701aec3c93d371117d1c
avidersonline-ux <avidersonline@gmail.com>
2025-12-06 22:33:06 +0530

    Update product_model.js

--- src/product_model.js
+++ src/product_model.js
@@ -1,17 +1,25 @@
 import mongoose from "mongoose";
 
 const ProductSchema = new mongoose.Schema({
-  id: { type: String, required: true },
+  id: String,  // ASIN
   title: String,
   brand: String,
-  image: String,
   price: Number,
   mrp: Number,
   currency: String,
   category: String,
-  source: String,
+  stock: String, // in_stock / out_of_stock
+
+  image: String,   // main image
+  images: [String], // gallery images
+
+  rating: Number,
+  reviews: Number,
+
   affiliateUrl: String,
-  updated_at: Date
+  source: String,  // amazon_in, amazon_us, internal
+
+  updated_at: Date,
 });
 
 export default mongoose.model("Product", ProductSchema);
