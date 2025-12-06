0eb1314a9c347af9a72abe2b46be8108b4c5e4d4
avidersonline-ux <avidersonline@gmail.com>
2025-12-06 21:55:38 +0530

    Update product_model.js

--- src/product_model.js
+++ src/product_model.js
@@ -1,7 +1,7 @@
 import mongoose from "mongoose";
 
 const ProductSchema = new mongoose.Schema({
-  id: String,
+  id: { type: String, required: true },
   title: String,
   brand: String,
   image: String,
