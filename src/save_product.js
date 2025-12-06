824f7f22c7e938ac7d72409ef7cca249a5274a3e
avidersonline-ux <avidersonline@gmail.com>
2025-12-07 01:21:07 +0530

    Update save_product.js

--- src/save_product.js
+++ src/save_product.js
@@ -1,9 +1,13 @@
 import { Product } from "./models/product_model.js";
 
 export async function saveProduct(p) {
-  return await Product.findOneAndUpdate(
-    { id: p.id },
-    { $set: p },
-    { upsert: true, new: true }
-  );
+  try {
+    await Product.findOneAndUpdate(
+      { id: p.id },         // match product by ASIN
+      { $set: p },          // set all updated fields
+      { upsert: true }      // create if missing
+    );
+  } catch (err) {
+    console.error("Error saving product:", err.message);
+  }
 }
