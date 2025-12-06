c9c0ddeab94bb2870c9fe4dc1788c3b54c663917
avidersonline-ux <avidersonline@gmail.com>
2025-12-06 19:59:13 +0530

    Update run_batch_us.js

--- src/run_batch_us.js
+++ src/run_batch_us.js
@@ -1,1 +1,26 @@
+import dotenv from "dotenv";
+dotenv.config();
+
+import { connectDB } from "./mongo_client.js";
+import { searchAmazon } from "./serpapi_client.js";
+import { normalizeProduct } from "./normalize_product.js";
+import { saveProduct } from "./save_product.js";
+
+import keywords from "./keywords_us.json" assert { type: "json" };
+
+await connectDB();
+
+for (let word of keywords) {
+  console.log("Searching US:", word);
+
+  const results = await searchAmazon(word, "us");
+
+  for (let raw of results) {
+    const p = normalizeProduct(raw, "us");
+    if (p) {
+      console.log("Saving:", p.id);
+      await saveProduct(p);
+    }
+  }
+}
 
