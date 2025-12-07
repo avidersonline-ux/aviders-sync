14cebbd7c291a399f453ac65c43c2836168283f5
avidersonline-ux <avidersonline@gmail.com>
2025-12-07 08:08:01 +0530

    Update ProductIN.js

--- src/models/ProductIN.js
+++ src/models/ProductIN.js
@@ -1,36 +1,29 @@
 import mongoose from "mongoose";
 
-const productINSchema = new mongoose.Schema(
+const ProductINSchema = new mongoose.Schema(
   {
-    id: { type: String, required: true, index: true, unique: true },
-
+    id: { type: String, required: true, unique: true },
     title: String,
     brand: String,
-
     price: Number,
     mrp: Number,
     currency: String,
-
     category: String,
     stock: String,
-
     rating: Number,
     reviews: Number,
-
     image: String,
     images: [String],
-
     bought_last_month: String,
     sponsored: Boolean,
     delivery: [String],
-
     source: String,
     affiliateUrl: String,
-
-    updated_at: Date
+    updated_at: Date,
   },
   { collection: "products_in" }
 );
 
-const ProductIN = mongoose.model("ProductIN", productINSchema);
-export default ProductIN;
+export default mongoose.models.ProductIN ||
+  mongoose.model("ProductIN", ProductINSchema);
+
